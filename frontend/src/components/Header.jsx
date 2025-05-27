889e0ba61e06f3c6f9ee5dc60c581c4999684451
Rafael <rafael.alves@eitv.com.br>
2025-05-27 12:59:43 -0300

    feat: estilizar o header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,31 @@
 import { Link } from "react-router-dom";
+import styles from './Header.module.css';
+import logo from '../assets/Símbolo 2.png';
 
-function Header(){
-    return(
-        <header style={{padding: 'irem', backgroundColor: '#eee'}}>
-            <nav>
-                <Link to="/" style={{marginRight: 'irem'}}>Início</Link>
-                <Link to="/campanhas" style={{marginRight: 'irem'}}>Campanhas</Link>
-            </nav>
-        </header>
-    );
+function Header() {
+  return (
+    <header className={styles.header}>
+      {/* Logo centralizada no topo */}
+      <div className={styles.logo}>
+        <img src={logo} alt="Juntaí" />
+      </div>
+
+      {/* Barra de navegação abaixo da logo */}
+      <div className={styles.navBar}>
+        <nav>
+          <ul className={styles.nav}>
+            <li className={styles.navItem}>
+              <Link to="/campanhas" className={styles.link}>Campanhas</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link to="/sobre" className={styles.link}>Sobre nós</Link>
+            </li>
+          </ul>
+        </nav>
+        <button className={styles.entrarBtn}>Entrar</button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
