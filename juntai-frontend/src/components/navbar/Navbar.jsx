a63f3b5195e5a7a74a8ea7f691ed2581b2cb2e73
Rafael <rafael.alves@eitv.com.br>
2025-07-29 15:47:25 -0300

    Melhorando a Navbar

--- juntai-frontend/src/components/navbar/Navbar.jsx
+++ juntai-frontend/src/components/navbar/Navbar.jsx
@@ -1,17 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 import './Navbar.css'
 import logo from '../../assets/Logo Horizontal 4.png'
 
 const Navbar = () => {
-    return(
-        <header className="header">
-            <a href=""><img src={logo} className="logo"/></a>
 
-            <nav className="navbar">
-                <a href="">Campanhas</a>
-                <a href="">Sobre nós</a>
-                <a href="">Entrar</a>
+    const [menuAtivo, setMenuAtivo] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuAtivo(!menuAtivo);
+    };
+
+    return(
+        <header>
+            <nav>
+                <a href="/">
+                <img src={logo} className="logo"/>
+                </a>
+
+                <div className={`mobile-menu ${menuAtivo ? 'active' : ''}`} onClick={toggleMenu}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+                <ul className={`nav-list ${menuAtivo ? 'active' : ''}`} onClick={toggleMenu}>
+                    <li><a href="#">Campanhas</a></li>
+                    <li><a href="#">Sobre nós</a></li>
+                    <li><a href="#">Entrar</a></li>
+                </ul>
             </nav>
+            
         </header>
     )
 }
